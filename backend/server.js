eaa1ae6848e9b25a63ec6bfd2e172a1d06ddec22
Ryan Deng <ryan.deng32@gmail.com>
2020-08-30 22:30:05 -0400

    finish 3.4

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,12 @@ app.get("/api/persons/:id", (req, res) => {
     res.status(404).end();
   }
 });
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
 app.get("/info", (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date()}</p>`);
